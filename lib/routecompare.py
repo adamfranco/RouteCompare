4905958ca32447cf474cdde583e721ef89ac081a
Adam Franco <afranco@middlebury.edu>
2015-03-25 21:28:35 -0400

    Basic config options are bing loaded.

---
+++ lib/routecompare.py
@@ -0,0 +1,17 @@
+
+class Route(object):
+	
+	label = "Unnamed Route"
+	bidirectional = False
+	
+	start_label = "Start"
+	start_latitude = 0
+	start_longitude = 0
+	start_radius = 100
+
+	end_label = "End"
+	end_latitude = 1
+	end_longitude = 1
+	end_radius = 100
+	
+	
